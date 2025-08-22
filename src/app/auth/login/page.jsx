1788b0a11315d71ba9c4f0907e2ef0656ff76d23
mjraihan <mjraihan000@gmail.com>
2025-08-23 06:47:23 +0700

    update meta auth

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -2,6 +2,11 @@ import { getSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import LoginForm from "@/components/Forms/LoginForm";
 
+export const metadata = {
+  title: "Login | Dashboard",
+  description: "Halaman login user",
+};
+
 export default async function LoginPage() {
   const session = await getSession();
   if (session?.user) {
